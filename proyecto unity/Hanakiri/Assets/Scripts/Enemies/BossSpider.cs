057039d22bdf229223f449b5f4125a7116db3a56
Ireealba <ireealba23@gmail.com>
2023-02-06 16:25:48 +0100

    boss Spider arreglada

--- proyecto unity/Hanakiri/Assets/Scripts/Enemies/BossSpider.cs
+++ proyecto unity/Hanakiri/Assets/Scripts/Enemies/BossSpider.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class BossSpider : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private PolygonCollider2D[] colliders;
+    private int currentColliderIndex = 0;
 
     void Start()
     {
@@ -27,5 +29,12 @@ public class BossSpider : MonoBehaviour
             Debug.Log("Player Damaged");
             Destroy(collision.gameObject);
         }
+    }
+
+    public void SetColliderForSprite(int spriteNum)
+    {
+        colliders[currentColliderIndex].enabled = false;
+        currentColliderIndex = spriteNum;
+        colliders[currentColliderIndex].enabled = true;
     }
 }
