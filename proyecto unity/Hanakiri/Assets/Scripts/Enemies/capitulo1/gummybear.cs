06286e3243cee2afb9f9e65df01fe3b5abee9095
Ireealba <ireealba23@gmail.com>
2023-02-26 18:52:52 +0100

    gummybear funcional

---
+++ proyecto unity/Hanakiri/Assets/Scripts/Enemies/capitulo1/gummybear.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gummybear : MonoBehaviour
+{
+    [SerializeField] private int tipo;
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer bear;
+    [SerializeField] private GameObject beargo;
+    [SerializeField] private GameObject todo;
+
+
+    [SerializeField] float speed = 0.5f;
+    private float waitTime;
+    [SerializeField] Transform[] moveSpots;
+    [SerializeField] float startWaitTime = 5;
+    private int i = 0;
+    private Transform oldspot;
+
+    void Start()
+    {
+        switch (tipo)
+        {
+            case 0:
+                animator.Play("red");
+                break;
+            case 1:
+                animator.Play("blue");
+                break;
+            case 2:
+                animator.Play("yellow");
+                break;
+            case 3:
+                animator.Play("green");
+                break;
+        }
+        waitTime = startWaitTime;
+        oldspot = transform;
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[i].transform.position, speed * Time.deltaTime);
+        if (moveSpots[i].position.x > oldspot.position.x)
+        {
+            bear.flipX = true;
+
+        }
+        else
+        {
+            bear.flipX = false;
+
+        }
+
+        if (waitTime <= 0)
+        {
+            if (moveSpots[i] != moveSpots[moveSpots.Length - 1])
+            {
+                i++;
+            }
+            else
+            {
+                i = 0;
+            }
+
+            waitTime = startWaitTime;
+        }
+        else
+        {
+            waitTime -= Time.deltaTime;
+        }
+
+        if (beargo == null)
+        {
+            Destroy(todo);
+        }
+    }
+}
---
+++ proyecto unity/Hanakiri/Assets/Scripts/Enemies/tutorial/WallSpider.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSpider : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+
+    [SerializeField] float speed = 0.5f;
+    private float waitTime;
+    [SerializeField] Transform[] moveSpots;
+    [SerializeField] float startWaitTime = 2;
+    private int i = 0;
+
+    [SerializeField] private PolygonCollider2D[] colliders;
+    private int currentColliderIndex = 0;
+
+    void Start()
+    {
+        waitTime = startWaitTime;
+        
+    }
+
+    void Update()
+    {
+
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[i].transform.position, speed * Time.deltaTime);
+
+        if(waitTime <= 0)
+        {
+            if (moveSpots[i] != moveSpots[moveSpots.Length - 1])
+            {
+                i++;
+            }
+            else
+            {
+                i = 0;
+            }
+
+            waitTime = startWaitTime;
+        }
+        else
+        {
+            waitTime -= Time.deltaTime;
+        }
+
+
+    }
+
+
+    public void SetColliderForSprite(int spriteNum)
+    {
+        colliders[currentColliderIndex].enabled = false;
+        currentColliderIndex = spriteNum;
+        colliders[currentColliderIndex].enabled = true;
+    }
+}
